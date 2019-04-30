adece1c5ceaf937b4049351c94188673281e01ad
Aodhan Hayter <aodhan.hayter@gmail.com>
2019-04-30 11:38:01 -0600

    Initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# Project Summary
+---
+Learn and understand how the JavaScript prototype chain works.
+
+## Setup
+
+1. Fork this repo
+2. clone your fork, then do all of the tasks in the following order.
+  1. instanceArray.js
+  2. sayName.js
+  3. arrayProperty.js
+  4. quizApp.js
---
+++ instanceArray.js
@@ -0,0 +1,41 @@
+/*
+  Your 3 Users will be the following.
+    0) Aodhan, aodhan@boom.camp, 'iLoveJS'
+    1) Greg, greg@boom.camp, 'iLovePython'
+    2) Oscar, oscar@boom.camp, 'iLoveSoccer'
+*/
+
+var User = function(name, email, pw){
+  this.name = name;
+  this.email = email;
+  this.pw = pw;
+}
+
+//Create an Array called 'users' that will store all our instances of User.
+
+  //code here
+
+
+//Now create and push into your users array 3 separate instances of User using the data from above in that exact order
+
+  //code here
+
+console.log('Aodhan\'s information is ');
+//Console.log all of Aodhan information
+
+  //code here
+
+console.log('Oscar\'s information is ');
+//Now console.log all of Oscars information
+
+  //code here
+
+
+//Now create another instance of User using your own information and then add that to your users array.
+
+  //code here
+
+console.log('All my users names are ');
+//Now loop through your users Array and console.log every users name.
+
+  //code here
