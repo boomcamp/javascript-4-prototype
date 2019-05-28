c6bcbab5808ba1057e45dc1d646f3e3e857bfba0
Trizha Kate Longaza <trizha.longaza@boom.camp>
2019-05-28 17:32:51 +0800

    javascript-4-prototype: completed

--- instanceArray.js
+++ instanceArray.js
@@ -14,11 +14,19 @@ var User = function(name, email, pw){
 //Create an Array called 'users' that will store all our instances of User.
 
   //code here
+  var users = [];
 
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
   //code here
+  var aodhan = ['Aodhan', 'aodhan@boom.camp', 'iLoveJS'];
+  users.push(aodhan);
+  var greg = ['Greg', 'greg@boom.camp', 'iLovePython'];
+  users.push(greg);
+  var oscar = ['Oscar', 'oscar@boom.camp', 'iLoveSoccer'];
+  users.push(greg);
+
 
 console.log('Aodhan\'s information is ');
 //Console.log all of Aodhan information
@@ -34,8 +42,13 @@ console.log('Oscar\'s information is ');
 //Now create another instance of User using your own information and then add that to your users array.
 
   //code here
+  var trizha = ['Trizha', 'trizha.longaza@boom.camp', 'iLoveCats'];
+  users.push(trizha);
 
 console.log('All my users names are ');
 //Now loop through your users Array and console.log every users name.
 
   //code here
+  for(let i = 0; i <= users.length-1; i++){
+    console.log(users[i]['name']);
+  }
