e2a37fca4cdc1151047044cdede9024130d16322
marvin banton <marvin.banton@boom.camp>
2019-05-28 15:29:44 +0800

    Finished problems in sayName.js

--- sayName.js
+++ sayName.js
@@ -1,13 +1,22 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
   //code here
-
+  var Person = function(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
 //Now create three instances of Person with data you make up
 
   //code here
-
+  var p1 = new Person('Aodhan', '27');
+  var p2 = new Person('Greg', '28');
+  var p3 = new Person('Oscar', '27');
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
   //code here
+  Person.prototype.sayName = function() {
+    alert(this.name);
+  }
+  
