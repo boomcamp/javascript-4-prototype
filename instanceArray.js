7b4badd77b5af2e91372fe338fd3b8411dfaa706
Sidney Bercasio <bercasio.sidney@gmail.com>
2019-05-28 14:20:38 +0800

    created an array called "users" that will store all our instances of User

--- instanceArray.js
+++ instanceArray.js
@@ -14,11 +14,12 @@ var User = function(name, email, pw){
 //Create an Array called 'users' that will store all our instances of User.
 
   //code here
-
+var users = [];
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
   //code here
+
 
 console.log('Aodhan\'s information is ');
 //Console.log all of Aodhan information
