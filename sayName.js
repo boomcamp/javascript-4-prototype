6b063abd3f0a3bda6d8c61308f4ac5b5cf3be4b2
Sidney Bercasio <bercasio.sidney@gmail.com>
2019-05-28 15:01:05 +0800

    done problem 3

--- sayName.js
+++ sayName.js
@@ -18,3 +18,6 @@ var person3 = new Person('Doe', 20);
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
   //code here
+Person.prototype.sayName = function(){
+  alert(this.name);
+}
