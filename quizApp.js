0f06587d98fc49268c6b29e29acf2703544838b6
marvinbanton <marvin.banton@boom.camp>
2019-05-28 17:28:36 +0800

    submission

--- quizApp.js
+++ quizApp.js
@@ -5,27 +5,39 @@
 
 //Create a QuizUser constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
-  //code here
+  function QuizUser(name,email,password,totalScore){
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.totalScore = totalScore;
+  }
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
-  //code here
+function Question(title,answersArray,rightAnswer,difficulty){
+  this.title = title;
+  this.answersArray = answersArray;
+  this.rightAnswer = rightAnswer;
+  this.difficulty = difficulty;
+}
 
 
 //Create a quizUsers Array which is going to hold all of our users.
 
-  //code here
+  var quizUsers = new Array();
 
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
-  //code here
-
+var Aodhan = new Array('Aodhan', 'aodhan@boom.camp', 'iLoveJS',90);
+var Greg = new Array('Greg', 'greg@boom.camp', 'iLovePython',90);
+var Oscar = new Array('Oscar', 'oscar@boom.camp', 'iLoveSoccer',75);
+quizUsers.push(Aodhan); quizUsers.push(Greg); quizUsers.push(Oscar);
 
 //Create a questions Array which is going to hold all of our questions
 
-  //code here
+var questions = new Array();
 
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
@@ -34,15 +46,20 @@
 //title: "T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value"
 //Fill in the rest of the required data as you see appropriate.
 
-  //code here
+var quest1 = new Array ('english','Inheritance is achieved in JavaScript through Prototypes?',true,'hard');
+var quest2 = new Array ('english','JavaScript is just a scripting version of Java',true,'easy');
+var quest3 = new Array ('english','n Javascript, == doesn\'t check \'type\' but just the value - where === checks type and value',true,'hard');
 
 
 //Now push all of your instances of Question into the questions Array
 
-  //code here
+questions.push(quest1);
+ questions.push(quest2); 
+ questions.push(quest3);
 
 console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
-  //code here
+    console.log(questions);
+    console.log(quizUsers);
 
--- sayName.js
+++ sayName.js
@@ -1,13 +1,20 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
-  //code here
-
-
+var Person = function(name, age){
+  this.name = name;
+  this.age = age;
+  this.sayName = function (){
+    alert(this.name);
+  }
+}
+ 
 //Now create three instances of Person with data you make up
 
-  //code here
+var Aodhan =new Person('Aodhan',30);
+var Greg =new Person('Greg',35);
+var Oscar =new Person('Oscar', 33);
 
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
-  //code here
+Aodhan.sayName();
