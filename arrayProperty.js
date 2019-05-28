4312c91050d8983076acf0368151415381e9a3bd
Koji Adriano Jr <rolando.adriano@boom.camp>
2019-05-28 16:34:41 +0800

    arrayProperty.js done, sayName.js done

--- arrayProperty.js
+++ arrayProperty.js
@@ -1,5 +1,9 @@
 //Just like you can add methods to your own constructor, you can also add methods and properties to built in classes in JavaScript like Arrays and Objects.
 
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
-  
-  //code here
+
+Person.prototype.reverseMe = function(name) {
+    var array = this.name;
+    array = array.split("").reverse().join("");
+    console.log(array);
+}
--- sayName.js
+++ sayName.js
@@ -11,6 +11,7 @@ var barbs = new Person('Elijah', '20');
 var jaako = new Person('Muhamad Jaako', '35');
 
 
+
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
 Person.prototype.sayName = function(name) {
