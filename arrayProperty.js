d40de79d7accdabf2e48555b048d9d6eb9a16d69
haceremorosa <hazel.remorosa@boom.camp>
2019-05-28 17:22:12 +0800

    Finished arrayProperty.js

--- arrayProperty.js
+++ arrayProperty.js
@@ -3,3 +3,15 @@
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
   //code here
+  function String(name) {
+    this.name = name;
+  }
+  var name1 = new String('Hazel');
+
+  String.prototype.reverse = function() {
+    var splitStr = this.name.split('');
+    var reverseStr = splitStr.reverse();
+    var joinStr = reverseStr.join('');
+
+    return joinStr;
+  }
