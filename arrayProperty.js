65841f36338a2faabdc2fbc405677d4b16bb89d7
dianageromo <diana.geromo@boom.camp>
2019-05-28 17:34:51 +0800

    Array Property

--- arrayProperty.js
+++ arrayProperty.js
@@ -1,5 +1,27 @@
-//Just like you can add methods to your own constructor, you can also add methods and properties to built in classes in JavaScript like Arrays and Objects.
+//Just like you can add methods to your own constructor, 
+//you can also add methods and properties to built in classes 
+//in JavaScript like Arrays and Objects.
 
-//Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
+//Add a reverse method to the String 'class' so that every 
+//instance of String can call reverse and reverse itself.
   
   //code here
+
+  
+  function String(toReverse){
+    this.toReverse = toReverse;
+  }
+  
+  
+  var string1 = new String("dianalyn");
+  
+  String.prototype.reverse = function(){ 
+    var toSplit = this.toReverse.split('');
+    var toRev = toSplit.reverse();
+    var toJoin = toRev.join('');
+  
+    return toJoin;
+  }
+  
+  
+  
