d484336e4bc87dfa391f5cdcba1ff9a219624feb
BS97 <jake.balbedina@boom.camp>
2019-10-16 14:20:39 +0800

    arrayProperty.js need to be fixed

--- arrayProperty.js
+++ arrayProperty.js
@@ -3,3 +3,10 @@
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
   //code here
+  // function String(word){
+  //   this.word = function(){
+  //       return word.split("").reverse().join(""); 
+  //   }
+  // }
+  
+  // var jake = new String("Jake");
--- instanceArray.js
+++ instanceArray.js
@@ -14,18 +14,22 @@ var User = function(name, email, pw){
 //Create an Array called 'users' that will store all our instances of User.
 
   //code here
+  var aodhan = new User("Aodhan", "aodhan@boom.camp", 'iLoveJS');
+  var greg = new User("Greg", "greg@boom.camp", 'iLovePython');
+  var oscar = new User("Oscar", "oscar@boom.camp", 'iLoveSoccer');
 
+  var users = [aodhan, greg, oscar];
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
   //code here
 
-console.log('Aodhan\'s information is ');
+console.log(`Aodhan\'s information is ${users[0]["name"]}, ${users[0]["email"]}, ${users[0]["pw"]}`);
 //Console.log all of Aodhan information
 
   //code here
 
-console.log('Oscar\'s information is ');
+console.log(`Oscar\'s information is ${users[1]["name"]}, ${users[1]["email"]}, ${users[1]["pw"]}`);
 //Now console.log all of Oscars information
 
   //code here
@@ -34,8 +38,13 @@ console.log('Oscar\'s information is ');
 //Now create another instance of User using your own information and then add that to your users array.
 
   //code here
+  users.push(jake = new User("Jake", "jake@boom.camp", 'iLovePh'));
+
 
 console.log('All my users names are ');
 //Now loop through your users Array and console.log every users name.
 
   //code here
+  for(i in users){
+    console.log(users[i]["name"])
+  }
