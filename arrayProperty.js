4c21d8a418785ba23e73ef407de0b23f43791a9c
dev-53 <jerommel.astillero@gmail.com>
2019-10-16 16:48:00 +0800

    Submission

--- arrayProperty.js
+++ arrayProperty.js
@@ -1,5 +1,15 @@
 //Just like you can add methods to your own constructor, you can also add methods and properties to built in classes in JavaScript like Arrays and Objects.
 
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
-  
-  //code here
+
+//code here
+String.prototype.reverse = function(arr) {
+  arr = this;
+  return arr
+    .split("")
+    .reverse()
+    .join("");
+};
+
+var arr = "rev";
+console.log(arr.reverse());
--- instanceArray.js
+++ instanceArray.js
@@ -5,37 +5,45 @@
     2) Oscar, oscar@boom.camp, 'iLoveSoccer'
 */
 
-var User = function(name, email, pw){
+var User = function(name, email, pw) {
   this.name = name;
   this.email = email;
   this.pw = pw;
-}
+};
 
 //Create an Array called 'users' that will store all our instances of User.
 
-  //code here
-
+//code here
+var users = [];
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
-  //code here
+//code here
+users[0] = new User("Aodhan", "aodhan@boom.camp", "iLoveJS");
+users[1] = new User("Greg", "greg@boom.camp", "iLovePython");
+users[2] = new User("Oscar", "oscar@boom.camp", "iLoveSoccer");
 
-console.log('Aodhan\'s information is ');
+console.log("Aodhan's information is ");
 //Console.log all of Aodhan information
 
-  //code here
+//code here
+console.log(users[0].name + " " + users[0].email + " " + users[0].pw);
 
-console.log('Oscar\'s information is ');
+console.log("Oscar's information is ");
 //Now console.log all of Oscars information
 
-  //code here
-
+//code here
+console.log(users[2].name + " " + users[2].email + " " + users[2].pw);
 
 //Now create another instance of User using your own information and then add that to your users array.
 
-  //code here
+//code here
+users[3] = new User("Jerommel", "astillero@boom.camp", "iLoveSoccer");
 
-console.log('All my users names are ');
+console.log("All my users names are ");
 //Now loop through your users Array and console.log every users name.
 
-  //code here
+//code here
+for (i in users) {
+  console.log(users[i].name);
+}
