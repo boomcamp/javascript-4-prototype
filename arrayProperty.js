a9fcd0e617ce462ffee4d26c21d7e246af7f1de4
bs98 <jeffrey.molleno@boom.camp>
2019-10-17 02:57:19 -0400

    Changes to spec

--- arrayProperty.js
+++ arrayProperty.js
@@ -3,10 +3,6 @@
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
   //code here
-
-  // class String extends String{
-
-  // }
   String.prototype.reverse = function(){
     return this.split("").reverse().join("");
   }
