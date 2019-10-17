ced1263728c04a00f2088509b2bfa1f3011e70eb
rapraquion <rapraquion@gmail.com>
2019-10-17 00:06:39 -0400

    sovled sayName.js

--- sayName.js
+++ sayName.js
@@ -1,11 +1,19 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
 //code here
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 //Now create three instances of Person with data you make up
 
 //code here
-
+var personOne = new Person("Aodhan", 28);
+var personTwo = new Person("Greg", 28);
+var personThree = new Person("Oscar", 28);
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
 //code here
+Person.prototype.sayName = function() {
+  alert(this.name);
+};
