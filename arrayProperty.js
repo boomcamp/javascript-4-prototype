99db32be156daf366347705f281bdf600c9dbbda
BS94 <you@example.com>
2019-10-17 02:09:26 -0400

    PROTOTYPE

--- arrayProperty.js
+++ arrayProperty.js
@@ -3,3 +3,13 @@
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
   //code here
+
+  String.prototype.reverse = function(str) {
+    str = this;
+    return str
+      .split("")
+      .reverse()
+      .join("");
+    };
+    var str = "datkilab";
+    console.log(str.reverse());
--- instanceArray.js
+++ instanceArray.js
@@ -1,41 +1,65 @@
-/*
-  Your 3 Users will be the following.
-    0) Aodhan, aodhan@boom.camp, 'iLoveJS'
-    1) Greg, greg@boom.camp, 'iLovePython'
-    2) Oscar, oscar@boom.camp, 'iLoveSoccer'
-*/
+  /*
+    Your 3 Users will be the following.
+      0) Aodhan, aodhan@boom.camp, 'iLoveJS'
+      1) Greg, greg@boom.camp, 'iLovePython'
+      2) Oscar, oscar@boom.camp, 'iLoveSoccer'
+  */
 
-var User = function(name, email, pw){
-  this.name = name;
-  this.email = email;
-  this.pw = pw;
-}
+  var User = function(name, email, pw){
+    this.name = name;
+    this.email = email;
+    this.pw = pw;
+  }
+  //Create an Array called 'users' that will store all our instances of User.
 
-//Create an Array called 'users' that will store all our instances of User.
+    //code here
+    let users = []; 
 
-  //code here
+  //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
+    //code here
+    users = [{
+      name: 'Aodhan',
+      email: 'aodhan@boom.camp',
+      pw: 'iLoveJS'
+    },
+    {
+      name: 'Greg',
+      email: 'greg@boom.camp',
+      pw: 'iLovePython'
+    },
+    {
+      name: 'Oscar',
+      email: 'oscar@boom.camp',
+      pw: 'iLoveSoccer'
+    }];
 
-//Now create and push into your users array 3 separate instances of User using the data from above in that exact order
+    console.log('Aodhan\'s information is ');
 
-  //code here
+  //Console.log all of Aodhan information
 
-console.log('Aodhan\'s information is ');
-//Console.log all of Aodhan information
+    //code here
+    console.log(users[0])
+    console.log('Oscar\'s information is ');
+  //Now console.log all of Oscars information
 
-  //code here
+    //code here
+    console.log(users[2])
 
-console.log('Oscar\'s information is ');
-//Now console.log all of Oscars information
+  //Now create another instance of User using your own information and then add that to your users array.
 
-  //code here
+    //code here
+    users[3] = {
+      name: 'Joven',
+      email: 'jovenbandagosa@boom.camp',
+      pw: 'iLoveJYOU'
+    }
+  console.log('All my users names are ');
+  //Now loop through your users Array and console.log every users name.
 
+    //code here
 
-//Now create another instance of User using your own information and then add that to your users array.
-
-  //code here
-
-console.log('All my users names are ');
-//Now loop through your users Array and console.log every users name.
-
-  //code here
+    for(let i = 0; i < users.length; i++)
+    {
+      console.log(users[i].name)
+    }
