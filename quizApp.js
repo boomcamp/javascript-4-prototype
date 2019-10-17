b8fc9297a5fbf341ffc1a0e4e4a061700a7afc2a
clarkamor <https://github.com/clarkamorr/learn-git.git>
2019-10-17 14:18:57 +0800

    my-javascript-4-prototype-clarkamor

--- quizApp.js
+++ quizApp.js
@@ -6,27 +6,39 @@
 //Create a QuizUser constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
   //code here
+  var QuizUser = function(name,email,password,totalScore){
+      this.name = name;
+      this.email = email;
+      this.password = password;
+      this.totalScore = totalScore;
+  }
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
   //code here
-
+var Question = function(title,answersArray,rightAnswer, difficulty){
+  this.title = title;
+  this.answersArray = answersArray;
+  this.rightAnswer = rightAnswer;
+  this.difficulty = difficulty;
+}
 
 //Create a quizUsers Array which is going to hold all of our users.
 
   //code here
-
+  var quizUsers = [];
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
   //code here
-
+  quizUsers.push("Aodhan","Greg","Oscar");
 
 //Create a questions Array which is going to hold all of our questions
 
   //code here
-
+  var questions = [];
+ 
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
 //title: 'T/F: Inheritance is achieved in JavaScript through Prototypes?'
@@ -35,12 +47,14 @@
 //Fill in the rest of the required data as you see appropriate.
 
   //code here
-
+  questions.push("Inheritance is achieved in JavaScript through Prototypes?");
+  questions.push("JavaScript is just a scripting version of Java");
+  questions.push("In Javascript, == doesn't check 'type' but just the value - where === checks type and value");
 
 //Now push all of your instances of Question into the questions Array
 
   //code here
-
+  
 console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
--- sayName.js
+++ sayName.js
@@ -1,13 +1,23 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
   //code here
-
+  var Person = function(name,age,sayName){
+      this.name = name;
+      this.age = age; 
+  
+  }
 
 //Now create three instances of Person with data you make up
 
   //code here
+    var Aodhan = new Person("Aodhan");
+    var Greg = new Person("Greg");
+    var Oscar = new Person("Oscar");
 
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
   //code here
+  Person.prototype.sayName = function() {
+   alert(this.name);
+};
