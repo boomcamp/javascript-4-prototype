ea066b7585e5e1580fd134a503c85cbd67ffeb72
rapraquion <rapraquion@gmail.com>
2019-10-17 00:01:38 -0400

    re-edit instanceArray.js

--- arrayProperty.js
+++ arrayProperty.js
@@ -1,5 +1,9 @@
 //Just like you can add methods to your own constructor, you can also add methods and properties to built in classes in JavaScript like Arrays and Objects.
 
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
-  
-  //code here
+
+//code here
+function reverse(str) {
+  let val = str.split("").join("");
+  return reverse;
+}
--- instanceArray.js
+++ instanceArray.js
@@ -4,7 +4,6 @@
     1) Greg, greg@boom.camp, 'iLovePython'
     2) Oscar, oscar@boom.camp, 'iLoveSoccer'
 */
-
 var User = function(name, email, pw) {
   this.name = name;
   this.email = email;
@@ -14,50 +13,37 @@ var User = function(name, email, pw) {
 //Create an Array called 'users' that will store all our instances of User.
 
 //code here
-let users = [
-  {
-    name: "Aodhan",
-    email: "aodhan@boom.camp",
-    pw: "iLoveJS"
-  },
-  {
-    name: "Greg",
-    email: "greg@boom.camp",
-    pw: "iLovePython"
-  },
-  {
-    name: "Oscar",
-    email: "oscar@boom.camp",
-    pw: "iLoveSoccer"
-  }
-];
+var users = [];
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
 //code here
-console.log(users.push[0]);
-console.log("Aodhan's information is ", users[0]);
+var firstInfo = new User("Aodhan", "aodhan@boom.camp", "iLoveJS");
+users.push(firstInfo);
+var secondInfo = new User("Greg", "greg@boom.camp", "iLovePython");
+users.push(secondInfo);
+var thirdInfo = new User("Oscar", "oscar@boom.camp", "iLoveSoccer");
+users.push(thirdInfo);
+
+console.log("Aodhan's information is ");
 //Console.log all of Aodhan information
 
 //code here
-console.log(users.push[1]);
-console.log("Oscar's information is ", users[2]);
+console.log(Object.values(firstInfo));
+console.log("Oscar's information is ");
 //Now console.log all of Oscars information
 
 //code here
-console.log(users.push[2]);
+console.log(Object.values(thirdInfo));
 //Now create another instance of User using your own information and then add that to your users array.
 
 //code here
-var user = [
-  {
-    name: "Rap",
-    email: "raphael.raquion@boom.camp",
-    pw: "imbatman"
-  }
-];
-console.log(users.push[3]);
+var fourthInfo = new User("Rap", "raphael.raquion@boom.camp", "imbatman");
+users.push(fourthInfo);
 console.log("All my users names are ");
 //Now loop through your users Array and console.log every users name.
 
 //code here
+for (let i in users) {
+  console.log(users[i].name);
+}
