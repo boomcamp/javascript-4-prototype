d7f22cb6db3f8b20fe5c6a567605dd402e72b9f2
rapraquion <rapraquion@gmail.com>
2019-10-16 23:07:57 -0400

    solved prob # 1&2 in instanceArray.js

--- instanceArray.js
+++ instanceArray.js
@@ -5,37 +5,59 @@
     2) Oscar, oscar@boom.camp, 'iLoveSoccer'
 */
 
-var User = function(name, email, pw){
+var User = function(name, email, pw) {
   this.name = name;
   this.email = email;
   this.pw = pw;
-}
+};
 
 //Create an Array called 'users' that will store all our instances of User.
 
-  //code here
-
+//code here
+let users = [
+  {
+    name: "Aodhan",
+    email: "aodhan@boom.camp",
+    pw: "iLoveJS"
+  },
+  {
+    name: "Greg",
+    email: "greg@boom.camp",
+    pw: "iLovePython"
+  },
+  {
+    name: "Oscar",
+    email: "oscar@boom.camp",
+    pw: "iLoveSoccer"
+  }
+];
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
-  //code here
-
-console.log('Aodhan\'s information is ');
+//code here
+console.log(users.push[0]);
+console.log("Aodhan's information is ", users[0]);
 //Console.log all of Aodhan information
 
-  //code here
-
-console.log('Oscar\'s information is ');
+//code here
+console.log(users.push[1]);
+console.log("Oscar's information is ", users[2]);
 //Now console.log all of Oscars information
 
-  //code here
-
-
+//code here
+console.log(users.push[2]);
 //Now create another instance of User using your own information and then add that to your users array.
 
-  //code here
-
-console.log('All my users names are ');
+//code here
+var user = [
+  {
+    name: "Rap",
+    email: "raphael.raquion@boom.camp",
+    pw: "imbatman"
+  }
+];
+console.log(users.push[3]);
+console.log("All my users names are ");
 //Now loop through your users Array and console.log every users name.
 
-  //code here
+//code here
--- test/spec/quizAppSpec.js
+++ test/spec/quizAppSpec.js
@@ -1,17 +1,17 @@
-const isNode = new Function(`
-  try {
-    return this === global;
-  } catch (e) {
-    return false;
-  }
-`);
+// const isNode = new Function(`
+//   try {
+//     return this === global;
+//   } catch (e) {
+//     return false;
+//   }
+// `);
 
 if (isNode()) {
   // test if file is running in a node process
-  const fs = require('fs');
-  const path = require('path');
+  const fs = require("fs");
+  const path = require("path");
 
-  const filePath = path.resolve(__dirname, '../../'); // this should be the root dir
+  const filePath = path.resolve(__dirname, "../../"); // this should be the root dir
   fs.readdirSync(filePath) // eval all of the js files faking how a browser would execute
     .filter(path => {
       if (path) {
@@ -21,64 +21,64 @@ if (isNode()) {
       }
     })
     .forEach(file => {
-      global.eval(fs.readFileSync(`${filePath}/${file}`) + '');
+      global.eval(fs.readFileSync(`${filePath}/${file}`) + "");
     });
 }
 
-describe('quizApp', function() {
-  describe('QuizUser', function() {
-    it('should exist', function() {
+describe("quizApp", function() {
+  describe("QuizUser", function() {
+    it("should exist", function() {
       expect(QuizUser).toBeDefined();
     });
-    it('should be a function', function() {
+    it("should be a function", function() {
       expect(QuizUser).toEqual(jasmine.any(Function));
     });
-    it('should return an object when called with new', function() {
-      var quizUser = new QuizUser('Tester', 'test', 'test', 0);
+    it("should return an object when called with new", function() {
+      var quizUser = new QuizUser("Tester", "test", "test", 0);
       expect(quizUser).toEqual(jasmine.any(Object));
     });
-    describe('return object', function() {
-      it('should have name, email, password, and totalScore properties', function() {
-        var quizUser = new QuizUser('Tester', 'test', 'test', 0);
-        expect(quizUser.hasOwnProperty('name')).toBe(true);
-        expect(quizUser.hasOwnProperty('email')).toBe(true);
-        expect(quizUser.hasOwnProperty('password')).toBe(true);
-        expect(quizUser.hasOwnProperty('totalScore')).toBe(true);
+    describe("return object", function() {
+      it("should have name, email, password, and totalScore properties", function() {
+        var quizUser = new QuizUser("Tester", "test", "test", 0);
+        expect(quizUser.hasOwnProperty("name")).toBe(true);
+        expect(quizUser.hasOwnProperty("email")).toBe(true);
+        expect(quizUser.hasOwnProperty("password")).toBe(true);
+        expect(quizUser.hasOwnProperty("totalScore")).toBe(true);
       });
     });
   });
-  describe('Question', function() {
-    it('should exist', function() {
+  describe("Question", function() {
+    it("should exist", function() {
       expect(Question).toBeDefined();
     });
-    it('should be a function', function() {
+    it("should be a function", function() {
       expect(Question).toEqual(jasmine.any(Function));
     });
-    it('should return an object when called with new', function() {
-      var question = new Question('Tester', 'test', 'test', 0);
+    it("should return an object when called with new", function() {
+      var question = new Question("Tester", "test", "test", 0);
       expect(question).toEqual(jasmine.any(Object));
     });
-    describe('return object', function() {
-      it('should have title, answersArray, rightAnswer, and difficulty properties', function() {
-        var question = new Question('Tester', 'test', 'test', 0);
-        expect(question.hasOwnProperty('title')).toBe(true);
-        expect(question.hasOwnProperty('answersArray')).toBe(true);
-        expect(question.hasOwnProperty('rightAnswer')).toBe(true);
-        expect(question.hasOwnProperty('difficulty')).toBe(true);
+    describe("return object", function() {
+      it("should have title, answersArray, rightAnswer, and difficulty properties", function() {
+        var question = new Question("Tester", "test", "test", 0);
+        expect(question.hasOwnProperty("title")).toBe(true);
+        expect(question.hasOwnProperty("answersArray")).toBe(true);
+        expect(question.hasOwnProperty("rightAnswer")).toBe(true);
+        expect(question.hasOwnProperty("difficulty")).toBe(true);
       });
     });
   });
-  describe('quizUsers', function() {
-    it('should exist', function() {
+  describe("quizUsers", function() {
+    it("should exist", function() {
       expect(quizUsers).toBeDefined();
     });
-    it('should be an array', function() {
+    it("should be an array", function() {
       expect(quizUsers).toEqual(jasmine.any(Array));
     });
-    it('should have three items', function() {
+    it("should have three items", function() {
       expect(quizUsers.length).toBe(3);
     });
-    it('(when complete) should only contain instances of QuizUser', function() {
+    it("(when complete) should only contain instances of QuizUser", function() {
       var areAllUsers = true;
       var quizUsers = quizUsers || [];
       for (var i = 0; i < quizUsers.length; i++) {
@@ -89,17 +89,17 @@ describe('quizApp', function() {
       expect(areAllUsers).toBe(true);
     });
   });
-  describe('questions', function() {
-    it('should exist', function() {
+  describe("questions", function() {
+    it("should exist", function() {
       expect(questions).toBeDefined();
     });
-    it('should be an array', function() {
+    it("should be an array", function() {
       expect(questions).toEqual(jasmine.any(Array));
     });
-    it('should have three items', function() {
+    it("should have three items", function() {
       expect(questions.length).toBe(3);
     });
-    it('(when complete) should only contain instances of Question', function() {
+    it("(when complete) should only contain instances of Question", function() {
       var areAllQuestions = true;
       var questions = questions || [];
       for (var i = 0; i < questions.length; i++) {
