39b5332d7469d1986a90134dda9ef1e24418e8fe
marcialnorte <56061640+marcialnorte@users.noreply.github.com>
2019-10-17 16:58:08 +0800

    Update sayNameSpec.js

--- test/spec/sayNameSpec.js
+++ test/spec/sayNameSpec.js
@@ -1,12 +1,11 @@
-const isNode = new Function(`
-  try {
-    return this === global;
-  } catch (e) {
-    return false;
-  }
-`);
-
-if (isNode()) {
+var isNode;
+if (typeof window === 'undefined') {
+    global.window = {node:true}
+    isNode = global.window.node;
+}else{
+    var isNode = false;
+}
+if (isNode) {
   // test if file is running in a node process
   const fs = require('fs');
   const path = require('path');
